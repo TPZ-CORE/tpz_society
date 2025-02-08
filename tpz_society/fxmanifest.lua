66888a5e72611a1042733c8f44b58f6d18579394
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2025-02-08 12:56:22 +0200

    Update fxmanifest.lua

--- tpz_society/fxmanifest.lua
+++ tpz_society/fxmanifest.lua
@@ -3,11 +3,17 @@ games {"rdr3"}
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
 author 'Nosmakos'
-description 'TPZ-CORE Society'
+description 'TPZ-CORE - Society'
 version '1.0.0'
 
 shared_scripts { 'config.lua', 'locales.lua' }
 client_scripts { 'client/*.lua' }
 server_scripts { 'server/*.lua' }
+
+dependencies {
+    'tpz_core',
+    'tpz_characters',
+    'tpz_inventory',
+}
 
 lua54 'yes'
