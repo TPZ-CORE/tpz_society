0ca766c3a4d27f97678bed8375388a15c2eaf9fb
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2025-05-22 01:33:14 +0300

    Delete tpz_society directory

--- tpz_society/client/tp-client_exports.lua
+++
@@ -1,9 +0,0 @@
-
------------------------------------------------------------
---[[ Exports  ]]--
------------------------------------------------------------
-
--- @HasActiveSocietyMenu : returns a boolean if player has active society menu (open) / not.
-exports('HasActiveSocietyMenu', function()
-    return GetPlayerData().HasMenuOpen
-end)
