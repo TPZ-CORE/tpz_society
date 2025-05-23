2b4e4fb790cc302c9d9ed4ab86e120b7fa8ccf23
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2025-05-23 10:59:08 +0300

    Add files via upload

---
+++ tpz_society/client/tp-client_duty.lua
@@ -0,0 +1,94 @@
+
+-----------------------------------------------------------
+--[[ Functions ]]--
+-----------------------------------------------------------
+
+function TogglePlayerDutyOnJoin()
+    local PlayerData = GetPlayerData()
+
+    if not Config.ToggleDutyOnPlayerJoin or Config.Societies[PlayerData.Job] == nil then
+        return
+    end
+
+    if not Config.Societies[PlayerData.Job].ActiveDuty then
+        return
+    end
+
+    TriggerServerEvent('tpz_society:server:togglePlayerDutyOnJoin')
+
+end
+
+-----------------------------------------------------------
+--[[ Threads ]]--
+-----------------------------------------------------------
+
+Citizen.CreateThread(function()
+    RegisterDutyActionPrompt()
+
+    while true do
+        Citizen.Wait(0)
+        
+        local sleep   = true
+        local player  = PlayerPedId()
+        local coords  = GetEntityCoords(player)
+
+        local isDead  = IsEntityDead(player)
+        local PlayerData = GetPlayerData()
+
+        if not isDead and not PlayerData.HasMenuOpen and PlayerData.Loaded then
+
+            local currentJobName = PlayerData.Job
+            if string.match(currentJobName, "off") then currentJobName = currentJobName:gsub("%off", "") end
+
+            for index, locConfig in pairs(Config.Societies) do
+
+                if locConfig.ActiveDuty then
+
+                    local playerDist  = vector3(coords.x, coords.y, coords.z)
+
+                    for _index, location in pairs (locConfig.DutyLocations) do
+                        local societyDist = vector3(location.Coords.x, location.Coords.y, location.Coords.z)
+                        local distance    = #(playerDist - societyDist)
+    
+                        if index == currentJobName then
+                            
+                            -- Creating marker on the location (If enabled).
+                            if locConfig.Marker.Enabled and distance <= locConfig.Marker.DisplayDistance then
+                                sleep = false
+                                local dr, dg, db, da = locConfig.Marker.RGBA.r, locConfig.Marker.RGBA.g, locConfig.Marker.RGBA.b, locConfig.Marker.RGBA.a
+                                Citizen.InvokeNative(0x2A32FAA57B937173, 0x94FDAE17, location.Coords.x, location.Coords.y, location.Coords.z - 1.0, 0, 0, 0, 0, 0, 0, 1.7, 1.7, 0.4, dr, dg, db, da, 0, 0, 2, 0, 0, 0, 0)
+    
+                            end
+        
+                            if distance <= locConfig.ActionDistance then
+    
+                                sleep = false
+            
+                                local promptGroup, promptList = GetDutyPromptData()
+
+                                local label = CreateVarString(10, 'LITERAL_STRING', Config.DutyPromptKey.label)
+                                PromptSetActiveGroupThisFrame(promptGroup, label)
+            
+                                if PromptHasHoldModeCompleted(promptList) then
+            
+                                    TriggerServerEvent('tpz_society:server:toggleDutyStatus')
+            
+                                    Wait(2000)
+                                end
+                            end
+    
+                        end
+
+                    end
+
+                end
+
+            end
+
+        end
+
+        if sleep then
+            Citizen.Wait(1000)
+        end
+    end
+end)
---
+++ tpz_society/client/tp-client_main.lua
@@ -0,0 +1,157 @@
+
+local PlayerData = { 
+    HasMenuOpen         = false,
+    Username            = nil,
+    CharIdentifier      = 0,
+    Job                 = nil,
+    JobGrade            = 0,
+    Loaded              = false
+}
+
+-----------------------------------------------------------
+--[[ Functions ]]--
+-----------------------------------------------------------
+
+function GetPlayerData()
+    return PlayerData
+end
+
+-----------------------------------------------------------
+--[[ Base Events & Threads ]]--
+-----------------------------------------------------------
+
+-- Gets the player job when devmode set to false and character is selected.
+AddEventHandler("tpz_core:isPlayerReady", function()
+
+    Wait(2000)
+    
+    local data = exports.tpz_core:getCoreAPI().getPlayerClientData()
+
+    if data == nil then
+        return
+    end
+
+    PlayerData.CharIdentifier = data.charIdentifier
+    PlayerData.Job            = data.job
+    PlayerData.JobGrade       = data.jobGrade
+
+    PlayerData.Username       = data.firstname .. ' ' .. data.lastname
+
+    PlayerData.Loaded         = true
+    
+    TriggerServerEvent('tpz_society:server:registerConnectedPlayer')
+    TriggerServerEvent("tpz_society:server:addChatSuggestions")
+        
+    TogglePlayerDutyOnJoin()
+
+end)
+
+-- Gets the player job when devmode set to true.
+if Config.DevMode then
+
+    Citizen.CreateThread(function ()
+
+        Wait(2000)
+
+        local data = exports.tpz_core:getCoreAPI().getPlayerClientData()
+
+        if data == nil then
+            return
+        end
+
+        PlayerData.CharIdentifier = data.charIdentifier
+        PlayerData.Job            = data.job
+        PlayerData.JobGrade       = data.jobGrade
+
+        PlayerData.Username       = data.firstname .. ' ' .. data.lastname
+
+        PlayerData.Loaded         = true
+    
+        TriggerServerEvent('tpz_society:server:registerConnectedPlayer')
+        TriggerServerEvent("tpz_society:server:addChatSuggestions")
+        
+        TogglePlayerDutyOnJoin()
+
+    end)
+end
+
+-- Updates the player job and job grade in case if changes.
+RegisterNetEvent("tpz_core:getPlayerJob")
+AddEventHandler("tpz_core:getPlayerJob", function(data)
+    PlayerData.Job      = data.job
+    PlayerData.JobGrade = data.jobGrade
+end)
+
+-----------------------------------------------------------
+--[[ Threads ]]--
+-----------------------------------------------------------
+
+Citizen.CreateThread(function()
+    RegisterActionPrompt()
+
+    while true do
+
+        Citizen.Wait(0)
+        
+        local sleep  = true
+        local player = PlayerPedId()
+        local coords = GetEntityCoords(PlayerPedId())
+
+        local isDead = IsEntityDead(player)
+
+        if not isDead and not PlayerData.HasMenuOpen and PlayerData.Loaded then
+
+            local isSecondaryInventoryActive = exports.tpz_inventory:getInventoryAPI().isSecondaryInventoryActive()
+
+            if not isSecondaryInventoryActive then
+
+                for index, locConfig in pairs(Config.Societies) do
+
+                    local playerDist  = vector3(coords.x, coords.y, coords.z)
+    
+                    for _index, location in pairs (locConfig.Locations) do
+                        local societyDist = vector3(location.Coords.x, location.Coords.y, location.Coords.z)
+                        local distance    = #(playerDist - societyDist)
+    
+                        if PlayerData.Job == index then
+                            
+                            -- Creating marker on the location (If enabled).
+                            if locConfig.Marker.Enabled and distance <= locConfig.Marker.DisplayDistance then
+                                sleep = false
+                                local dr, dg, db, da = locConfig.Marker.RGBA.r, locConfig.Marker.RGBA.g, locConfig.Marker.RGBA.b, locConfig.Marker.RGBA.a
+                                Citizen.InvokeNative(0x2A32FAA57B937173, 0x94FDAE17, location.Coords.x, location.Coords.y, location.Coords.z - 1.0, 0, 0, 0, 0, 0, 0, 1.7, 1.7, 0.4, dr, dg, db, da, 0, 0, 2, 0, 0, 0, 0)
+    
+                            end
+        
+                            if distance <= locConfig.ActionDistance then
+    
+                                sleep = false
+            
+                                local promptGroup, promptList = GetPromptData()
+    
+                                local label = CreateVarString(10, 'LITERAL_STRING', Config.PromptKey.label)
+                                PromptSetActiveGroupThisFrame(promptGroup, label)
+            
+                                if PromptHasHoldModeCompleted(promptList) then
+            
+                                    OpenSocietyManagementMenu(_index)
+            
+                                    Wait(2000)
+                                end
+                            end
+    
+                        end
+    
+                    end
+    
+                end
+
+            end
+
+        end
+
+        if sleep then
+            Citizen.Wait(1000)
+        end
+    end
+end)
