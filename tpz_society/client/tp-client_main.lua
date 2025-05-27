a093a0a35fba3ed9fd21ebcd086d1edb1ccabf0c
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2025-05-27 12:09:35 +0300

    Update tp-client_main.lua

--- tpz_society/client/tp-client_main.lua
+++ tpz_society/client/tp-client_main.lua
@@ -25,7 +25,7 @@ AddEventHandler("tpz_core:isPlayerReady", function()
 
     Wait(2000)
     
-    local data = exports.tpz_core:getCoreAPI().getPlayerClientData()
+    local data = exports.tpz_core:getCoreAPI().GetPlayerClientData()
 
     if data == nil then
         return
@@ -53,7 +53,7 @@ if Config.DevMode then
 
         Wait(2000)
 
-        local data = exports.tpz_core:getCoreAPI().getPlayerClientData()
+        local data = exports.tpz_core:getCoreAPI().GetPlayerClientData()
 
         if data == nil then
             return
