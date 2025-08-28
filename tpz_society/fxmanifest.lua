0269b25fc32efa2661bc3f051a1dfdce1ca259a5
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2025-08-28 12:58:57 +0300

    Update fxmanifest.lua

--- tpz_society/fxmanifest.lua
+++ tpz_society/fxmanifest.lua
@@ -4,7 +4,7 @@ rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aw
 
 author 'Nosmakos'
 description 'TPZ-CORE - Society'
-version '1.0.0'
+version '1.0.1'
 
 ui_page 'html/index.html'
 
