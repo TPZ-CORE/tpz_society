cd1449a4ef667f5c6a3a1c3adbe7510c2888c620
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2025-09-12 13:50:16 +0300

    Update tp-client_duty.lua

--- tpz_society/client/tp-client_duty.lua
+++ tpz_society/client/tp-client_duty.lua
@@ -1,3 +1,4 @@
+local hasThreadActive = false
 
 -----------------------------------------------------------
 --[[ Functions ]]--
@@ -24,71 +25,81 @@ end
 
 Citizen.CreateThread(function()
     RegisterDutyActionPrompt()
+end)
 
-    while true do
-        Citizen.Wait(0)
-        
-        local sleep   = true
-        local player  = PlayerPedId()
-        local coords  = GetEntityCoords(player)
+AddEventHandler('tpz_society:client:start_duty_thread', function()
+    local currentJobName = GetPlayerData().Job
+    if string.match(currentJobName, "off") then currentJobName = currentJobName:gsub("%off", "") end
 
-        local isDead  = IsEntityDead(player)
-        local PlayerData = GetPlayerData()
+    if (Config.Societies[currentJobName] == nil) or (Config.Societies[currentJobName] and not Config.Societies[currentJobName].ActiveDuty) or hasThreadActive then
+        return
+    end
 
-        if not isDead and not PlayerData.HasMenuOpen and PlayerData.Loaded then
+    hasThreadActive = true
+
+    Citizen.CreateThread(function()
+
+        while true do
+
+            local sleep      = 1000
+            local player     = PlayerPedId()
+
+            local isDead     = IsEntityDead(player)
+            local PlayerData = GetPlayerData()
 
             local currentJobName = PlayerData.Job
             if string.match(currentJobName, "off") then currentJobName = currentJobName:gsub("%off", "") end
-
-            for index, locConfig in pairs(Config.Societies) do
-
-                if locConfig.ActiveDuty then
-
-                    local playerDist  = vector3(coords.x, coords.y, coords.z)
-
-                    for _index, location in pairs (locConfig.DutyLocations) do
-                        local societyDist = vector3(location.Coords.x, location.Coords.y, location.Coords.z)
-                        local distance    = #(playerDist - societyDist)
-    
-                        if index == currentJobName then
-                            
-                            -- Creating marker on the location (If enabled).
-                            if locConfig.Marker.Enabled and distance <= locConfig.Marker.DisplayDistance then
-                                sleep = false
-                                local dr, dg, db, da = locConfig.Marker.RGBA.r, locConfig.Marker.RGBA.g, locConfig.Marker.RGBA.b, locConfig.Marker.RGBA.a
-                                Citizen.InvokeNative(0x2A32FAA57B937173, 0x94FDAE17, location.Coords.x, location.Coords.y, location.Coords.z - 1.0, 0, 0, 0, 0, 0, 0, 1.7, 1.7, 0.4, dr, dg, db, da, 0, 0, 2, 0, 0, 0, 0)
-    
-                            end
         
-                            if distance <= locConfig.ActionDistance then
+            if (Config.Societies[currentJobName] == nil) or (Config.Societies[currentJobName] and not Config.Societies[currentJobName].ActiveDuty) then
+                hasThreadActive = false 
+                break 
+            end
     
-                                sleep = false
-            
-                                local promptGroup, promptList = GetDutyPromptData()
+            if isDead or PlayerData.HasMenuOpen then
+                goto END
+            end
 
-                                local label = CreateVarString(10, 'LITERAL_STRING', Config.DutyPromptKey.label)
-                                PromptSetActiveGroupThisFrame(promptGroup, label)
-            
-                                if PromptHasHoldModeCompleted(promptList) then
-            
-                                    TriggerServerEvent('tpz_society:server:toggleDutyStatus')
-            
-                                    Wait(2000)
-                                end
-                            end
+            if not isDead and not PlayerData.HasMenuOpen then
+
+                local coords    = GetEntityCoords(player)
+                local locConfig = Config.Societies[currentJobName]
     
+                for _index, location in pairs (locConfig.DutyLocations) do
+
+                    local distance = #(coords - vector3(location.Coords.x, location.Coords.y, location.Coords.z))
+
+                    -- Creating marker on the location (If enabled).
+                    if locConfig.Marker.Enabled and distance <= locConfig.Marker.DisplayDistance then
+                        sleep = 0
+                        local dr, dg, db, da = locConfig.Marker.RGBA.r, locConfig.Marker.RGBA.g, locConfig.Marker.RGBA.b, locConfig.Marker.RGBA.a
+                        Citizen.InvokeNative(0x2A32FAA57B937173, 0x94FDAE17, location.Coords.x, location.Coords.y, location.Coords.z - 1.0, 0, 0, 0, 0, 0, 0, 1.7, 1.7, 0.4, dr, dg, db, da, 0, 0, 2, 0, 0, 0, 0)
+                    end
+
+                    if distance <= locConfig.ActionDistance then
+
+                        sleep = 0
+    
+                        local promptGroup, promptList = GetDutyPromptData()
+
+                        local label = CreateVarString(10, 'LITERAL_STRING', Config.DutyPromptKey.label)
+                        PromptSetActiveGroupThisFrame(promptGroup, label)
+    
+                        if PromptHasHoldModeCompleted(promptList) then
+    
+                            TriggerServerEvent('tpz_society:server:toggleDutyStatus')
+    
+                            Wait(2000)
                         end
-
                     end
 
                 end
-
+    
             end
-
+    
+            ::END::
+            Wait(sleep)
         end
 
-        if sleep then
-            Citizen.Wait(1000)
-        end
-    end
+    end)
+
 end)
